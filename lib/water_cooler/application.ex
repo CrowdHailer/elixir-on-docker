2b01030c28e7984cf4d2940ea212317372621dac
CrowdHailer <peterhsaxton@gmail.com>
2017-04-29 18:14:42 +0100

    Add ssl setup to water cooler example

--- lib/water_cooler/application.ex
+++ lib/water_cooler/application.ex
@@ -6,13 +6,23 @@ defmodule WaterCooler.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    certificate_path =
+      Application.app_dir(:water_cooler, "priv/localhost/certificate.pem")
+    certificate_key_path =
+      Application.app_dir(:water_cooler, "priv/localhost/certificate_key.pem")
+
+    app = {WaterCooler.WWW, []}
+
     children = [
-      worker(Ace.HTTP, [{WaterCooler.WWW, []}, [port: 8080, name: WaterCooler.WWW]]),
-    #   worker(Ace.HTTPS, [{WaterCooler.WWW, :config}, [
-    #     port: 8443,
-    #     certificate: Application.app_dir(:water_cooler, "priv/localhost.crt"),
-    #     certificate_key: Application.app_dir(:water_cooler, "priv/localhost.key")
-    #   ]])
+      worker(Ace.HTTP, [app, [
+        port: 8080,
+        name: WaterCooler.WWW
+      ]]),
+      worker(Ace.HTTPS, [app, [
+        port: 8443,
+        certificate: certificate_path,
+        certificate_key: certificate_key_path
+      ]])
     ]
 
     opts = [strategy: :one_for_one, name: WaterCooler.Supervisor]
