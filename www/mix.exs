fa5ab484b42ddbffb776033e8744ac361cf5561d
CrowdHailer <peterhsaxton@gmail.com>
2017-10-29 10:04:06 +0000

    fix wobserver and exsync

--- www/mix.exs
+++ www/mix.exs
@@ -3,8 +3,7 @@ defmodule WWW.Mixfile do
 
   def project do
     [
-      # TODO make :www
-      app: :water_cooler,
+      app: :www,
       version: "0.0.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
@@ -21,7 +20,7 @@ defmodule WWW.Mixfile do
     [
       {:ace, "~> 0.15.0"},
       {:server_sent_event, "~> 0.2.0"},
-      {:exsync, "~> 0.2.0"},
+      {:exsync, "~> 0.2.1"},
       {:wobserver, "~> 0.1.8"}
     ]
   end
